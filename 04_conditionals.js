62da09ed4f9bdf345c9348e1e305f243323f0112
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-15 01:36:50 +0500

    conditionals in js

---
+++ 04_conditionals.js
@@ -0,0 +1,22 @@
+if("hamza"==="hamza"){
+    console.log("i am hamza");
+}
+
+if(2==3){
+    console.log("2 is equal to 3");
+    
+}else if(2==2){
+    
+    console.log("2 is equal to 2");
+} else{
+    console.log("no cond matched");
+    
+}
+
+if(2==3){
+    console.log("2 is equal to 3");
+    
+}else{
+    console.log("no cond matched");
+
+}
