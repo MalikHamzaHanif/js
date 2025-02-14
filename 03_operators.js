aaeed83f582229e126ef7f394dd432fd8f9a8f15
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-15 01:31:08 +0500

    operators in js

---
+++ 03_operators.js
@@ -0,0 +1,6 @@
+console.log(2+2);//addition
+console.log(2-2);//subtration
+console.log(2*2);//mulitiplication
+console.log(2/2);//division
+console.log(2%2);//remainder
+console.log(2**3);//2 to the power 2 .. 2*2*2
