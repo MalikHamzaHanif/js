5a1b4da2c5c35d0d76640208035e709d84a91e0a
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-15 01:33:29 +0500

    operators in js

--- 03_operators.js
+++ 03_operators.js
@@ -4,3 +4,21 @@ console.log(2*2);//mulitiplication
 console.log(2/2);//division
 console.log(2%2);//remainder
 console.log(2**3);//2 to the power 2 .. 2*2*2
+
+//comparison operators 
+
+console.log(2==="2");
+console.log(2=="2");
+console.log(2!=2);
+console.log(2!="2");
+
+//logical operators
+
+console.log(true||false);//true
+console.log(true&&true); //true
+
+
+
+
+
+
