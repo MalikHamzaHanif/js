18efca3084cb03ded6369032ee842b5d0a1e5b9d
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-15 01:53:59 +0500

    string methods in js

---
+++ 07_strings.js
@@ -0,0 +1,32 @@
+let sentence="i am a sentence and i contain some words... is is ";
+
+console.log(sentence);
+console.log(`here is the sentence ${sentence}`);//template literals
+console.log(sentence.toUpperCase());
+console.log("THIS IS A CHAIR".toLowerCase());
+console.log(sentence.length);
+console.log(sentence.match("is"));
+console.log(sentence.includes("is"));
+console.log(sentence.substring(3));
+console.log(sentence.substring(3,9));
+console.log(sentence.trim());
+console.log(sentence.slice(2,7));
+console.log(sentence.concat("this is another sentence"));
+console.log(sentence.charAt(5));
+console.log(sentence.startsWith("is"));
+console.log(sentence.endsWith(" "));
+console.log(sentence.replace("is","nothing"));
+console.log(sentence.replaceAll("is","nothing"));
+
+
+
+
+
+
+
+
+
+
+
+
+
