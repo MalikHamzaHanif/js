2b6f6ea90f9deabf2dff4179bd77b8e9c24db552
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-15 01:47:37 +0500

    fucntions in j/s

---
+++ 06_functions.js
@@ -0,0 +1,15 @@
+function subtractValues(a,b){
+    return a-b;
+}
+
+console.log(subtractValues(56,6));
+
+let subValues=(a,b)=>a-b;
+
+console.log(subValues(56,6));
+
+let addValues=(a,b)=>{return a+b};
+
+console.log(addValues(50,6));
+
+
