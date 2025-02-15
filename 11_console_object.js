122d3f28256e4721a54dd969a24e6f02b5b078c1
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-15 22:40:47 +0500

    console object in js

---
+++ 11_console_object.js
@@ -0,0 +1,9 @@
+console.clear();
+
+let arr=[11,22,33,44]
+console.log("hi i am hamza");
+console.info("this is some information");
+console.error("this is error message");
+console.table(arr);
+console.warn("warning message");
+console.assert(5-5>4);//asertion failed 
