541c39c7b8028a55ac3d31370c69c91355e0e228
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-17 01:50:33 +0500

    settimeout and setinterval in js

---
+++ 20_settimeout_and_setinterval.js
@@ -0,0 +1,10 @@
+
+setTimeout(()=>{
+    console.log("i will get printed after 2 sec");
+    
+},2000);
+
+setInterval(()=>{
+    console.log("after every 2 sec ill get printed on the screen");
+    
+},2000);
