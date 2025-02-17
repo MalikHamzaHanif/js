9c9a9cda95505d56c43d177ef4139987c9c11a03
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-18 01:55:28 +0500

    promises in js

---
+++ 26_promises.js
@@ -0,0 +1,70 @@
+// const promiseOne = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (true === true) {
+//             console.log("resolving the promise");
+
+//             resolve("resolved with some data");
+//         } else {
+//             console.log("rejecting the promise");
+//             reject("something went wrong");
+//         }
+//     }, 2000);
+// });
+
+// promiseOne.then((data)=>{
+//     console.log(data);
+
+// }).catch((e)=>{
+//     console.log("some error");
+
+// }).finally(()=>{console.log("promise any how resolved or rejected");
+// })
+
+
+// const promiseTwo=new Promise((resolve ,reject)=>{
+//    const data= fetch("https://jsonplaceholder.typicode.com/todos");
+//    resolve(data);
+// })
+
+
+// promiseTwo.then((data)=>{
+//     console.log(data);
+
+//     // return new Promise((resolve, reject) => {
+//     //     resolve(data.json());
+//     // })
+
+//     return data.json()
+
+// }).then((data)=>{
+//     console.log(data);
+
+// })
+
+
+
+async function getData() {
+    try {
+        const data = await fetch("https://jsonplaceholder.typicode.com/todos");
+        const todo = await data.json();
+        // console.log(todo);
+        return todo;
+    } catch (e) {
+        console.log(e);
+
+    }
+
+
+}
+
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/todos").then((data) => {
+//     return data.json()
+// }).then((data) => {
+//     console.log(data);
+
+// }).catch((e) => {
+//     console.log(e);
+// })
